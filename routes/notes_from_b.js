ab873e6bc665eeded28a7d88dfda2ee389290e51
daiki69 <daiki6guitar@gmail.com>
2024-01-26 22:14:46 +0900

    notescommit

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
-
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://daiki6guitar:xBT6tbIrGJP5eUgC@test.gloelrv.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
   try {
@@ -19,9 +19,6 @@ router.get('/', async (req, res) => {
     const note = await notes.find({}).toArray();
 
     res.json(note);
-  } catch (error) {
-    console.error("エラーが発生しました:", error);
-    res.status(500).json({ error: "Internal Server Error" });
   } finally {
     // 接続をクローズ
     await client.close();
