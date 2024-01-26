991ccfa221d28416a36e9946e1b4a04294942b98
daiki69 <daiki6guitar@gmail.com>
2024-01-26 23:33:05 +0900

    add env

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'HelloExpress' });
+  res.render('hello!world', { title: 'HelloExpress!!' });
 });
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://daiki6guitar:xBT6tbIrGJP5eUgC@test.gloelrv.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
